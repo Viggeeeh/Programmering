30d75022aa8d359e58ecc6d4516f2359adb4564e
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2023-08-22 09:53:52 +0200

    Modul 2 klar

--- Modul 2/app.py
+++ Modul 2/app.py
@@ -37,3 +37,16 @@ elif (bmi >= 25 and bmi < 40):
     print("bmi = ", bmi, "du är överviktig.")
 else: 
     print("bmi = ", bmi, "du är väldigt överviktig.")
+
+# Uppgift 6
+# (2023 - 2006) * 12 * 4
+year_born = int(input("Viket år är du född? "))
+age = 2023 - year_born
+months_alive = age * 12
+weeks_alive = months_alive * 4
+print("Du har levt", weeks_alive)
+
+# Uppgift 7
+weight_kg = float(input("Skriv en vikt i kg: "))
+converted_weight = weight_kg * 2.2
+print(weight_kg, "in kg is", converted_weight, "in lbs")
