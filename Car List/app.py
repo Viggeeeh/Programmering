419d4fe3789e0cd3a9ecb619ef469e5e8d554093
Viggeeeh <viggo.ofors@elev.ga.ntig.se>
2023-10-05 11:44:46 +0200

    Klart

--- Car List/app.py
+++ Car List/app.py
@@ -4,6 +4,7 @@ from colors import bcolors
 os.system("cls")
 car_list = []
 
+
 def instructions():
     print(f"""{bcolors.DEFAULT}Welcome to {bcolors.BOLD + bcolors.CYAN}Favourite Cars List{bcolors.DEFAULT}
 1) Type a new car and it will {bcolors.UNDERLINE + bcolors.GREEN}appear{bcolors.DEFAULT}
@@ -11,26 +12,25 @@ def instructions():
 3) Write {bcolors.BOLD + bcolors.BLUE}edit {bcolors.DEFAULT}to edit a list item
 4) Press {bcolors.BOLD + bcolors.PURPLE}enter{bcolors.DEFAULT} to {bcolors.UNDERLINE}exit{bcolors.DEFAULT} from the list\n""")
     
-
-def check_inputs(name):
-    if name == "":
+def check_inputs(car_name):
+    if car_name == "":
         exit()
 
-    if name == "edit":
+    if car_name == "edit" and len(car_list) > 0:
         while True: # Makes sure we don't get a wrong input
             try:
-                edit_car = int(input(f"{bcolors.DEFAULT}What item would you like to edit? "))
+                edit_car = int(input(f"{bcolors.DEFAULT}What item would you like to edit? (1 - {len(car_list)}): "))
                 new_car = input(f"What would you like to change {bcolors.BLUE + car_list[edit_car-1] + bcolors.DEFAULT} with? ")
                 car_list[edit_car-1] = new_car
                 break
             except:
                 print("It has to be in the list!\n")
             
-    elif name in car_list:
-        car_list.pop(car_list.index(name)) 
+    elif car_name in car_list:
+        car_list.pop(car_list.index(car_name)) 
 
-    elif not name in car_list: 
-        car_list.append(name)
+    elif not car_name in car_list: 
+        car_list.append(car_name)
 
 
 while True:
